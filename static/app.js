cf5774ffc63666243af88e13acd3f9045f691f55
mpuk <puk.matej@gmail.com>
2017-11-01 15:52:45 +0100

    added button for geolocation to map

--- static/app.js
+++ static/app.js
@@ -1,10 +1,21 @@
 $( document ).ready(function() {
-	var map = L.map('mapid').setView([48.6905689, 19.4581682], 8);
+	map = L.map('mapid').setView([48.6905689, 19.4581682], 8);
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     	maxZoom: 18,
     	id: 'mapbox.streets',
     	accessToken: 'pk.eyJ1IjoibXB1ayIsImEiOiJjajgwMzViM2k2OG1jMnFudHFpZWFpcDBuIn0.tJeJccXTdskhNbv2IS5kAQ'
+	}).addTo(map);
+
+	L.easyButton('<img src="static/icons/location.png">', function (btn, map) {
+    	map.locate({
+        	setView: true,
+        	maxZoom: 13
+    	});
+
+    	map.on('locationfound', function(e) {
+    		// save coordinates
+		});
 	}).addTo(map);
 });
