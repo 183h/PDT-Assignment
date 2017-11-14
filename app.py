877ddf6e358f347c07c4f60f0c2c1853d87ec01a
mpuk <puk.matej@gmail.com>
2017-11-14 14:13:47 +0100

    added api for getting amenities near hike

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # This Python file uses the following encoding: utf-8
 
 from flask import Flask, render_template, jsonify
-from psycopg2 import connect
+from psycopg2 import connect, sql
 
 app = Flask(__name__)
 
@@ -39,6 +39,47 @@ def api_get_hiking():
             ) \
             AS f ) \
         AS fc;"
+    )
+
+    response = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return jsonify(data=response)
+
+
+@app.route("/api/get/amenities/<hike>")
+def api_get_amenities(hike):
+    conn = connect("dbname=gis user=postgres")
+    cur = conn.cursor()
+
+    cur.execute(
+        sql.SQL(
+            "with hike as( \
+                SELECT ST_Collect(l.way) AS geometry, name \
+                FROM planet_osm_line as l \
+                WHERE \
+                    l.route = 'hiking' \
+                    and l.name = %s \
+                GROUP by l.name, l.operator \
+            ) \
+            SELECT row_to_json(fc) \
+            FROM ( \
+                SELECT 'FeatureCollection' AS type, array_to_json(array_agg(f)) AS features \
+                FROM ( \
+                    SELECT 'Feature' AS type, ST_AsGeoJSON(ST_Transform(p.way, 4326))::json AS geometry,\
+                    row_to_json((p.name, p.historic)) AS properties \
+                    from hike as h \
+                    cross join planet_osm_point as p \
+                    where \
+                        p.historic = 'memorial' \
+                        and p.name is not null \
+                        and ST_DWithin(h.geometry, p.way, 5000) \
+                ) AS f \
+            ) AS fc;"
+        ),
+        [hike]
     )
 
     response = cur.fetchall()
