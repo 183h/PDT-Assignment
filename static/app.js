d3e32d011104f2c164990a971ddff4894e72e2e8
mpuk <puk.matej@gmail.com>
2017-11-17 17:55:43 +0100

    added markers for amenities; fixed zooming for hikes on other pages of list; added amenities layer to map

--- static/app.js
+++ static/app.js
@@ -1,8 +1,70 @@
 $(document).ready(function() {
+	// amenities markers
+	var markerMemorial = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'university', markerColor: 'red'});
+	var markerYes = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'map-pin', markerColor: 'red'});
+	var markerBoundaryStone = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'bandcamp', markerColor: 'red'});
+	var markerWaysideShrine = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'child', markerColor: 'red' });
+	var markerMonument = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'trophy', markerColor: 'red'});
+	var markerWaysideCross = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'arrows', markerColor: 'red'});
+	var markerCastle = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'shield',	markerColor: 'red'});
+	var markerRuins = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'bookmark', markerColor: 'red'});
+	var markerArcheologicalSite = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'paint-brush', markerColor: 'red'});
+	var markerBattlefield = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'crosshairs', markerColor: 'red'});
+	var markerBuilding = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'building',	markerColor: 'red'});
+	var markerMine = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'industry',	markerColor: 'red'});
+	var markerManor = L.AwesomeMarkers.icon({prefix: 'fa',icon: 'diamond', markerColor: 'red'});
+	var markerTank = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'car', markerColor: 'red'});
+	var markerCannon = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'rocket',	markerColor: 'red'});
+	var markerBunker = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'crosshairs', markerColor: 'red'});
+	var markerLocomotive = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'train', markerColor: 'red'});
+	var markerTomb = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'arrows', markerColor: 'red'});
+	var markerGrave = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'arrows', markerColor: 'red'});
+	var markerChurch = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'child', markerColor: 'red'});
+	var markerBridge = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'arrows-hd', markerColor: 'red'});
+	var markerCityGate = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'film', markerColor: 'red'});
+	var markerPillory = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'bars', markerColor: 'red'});
+	var markerPlaceOfWorship = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'child',markerColor: 'red'});
+	var markerChateau = L.AwesomeMarkers.icon({prefix: 'fa', icon: 'diamond', markerColor: 'red'});
+	var markerChalet = L.AwesomeMarkers.icon({prefix: 'fa',	icon: 'home', markerColor: 'red'});
+	var markerBakery = L.AwesomeMarkers.icon({prefix: 'fa',	icon: 'birthday-cake', markerColor: 'red'});
+	var markerBorderControl = L.AwesomeMarkers.icon({ prefix: 'fa',	icon: 'map', markerColor: 'red'});
+
+	markers = {
+		'memorial' : markerMemorial,
+		"yes" : markerYes,
+		"boundary_stone" : markerBoundaryStone,
+		"wayside_shrine" : markerWaysideShrine,
+		"monument" : markerMonument,
+		"wayside_cross" : markerWaysideCross,
+		"castle" : markerCastle,
+		"ruins" : markerRuins,
+		"archaeological_site" : markerArcheologicalSite,
+		"battlefield" : markerBattlefield,
+		"building" : markerBuilding,
+		"mine" : markerMine,
+		"manor" : markerManor,
+		"tank" : markerTank,
+		"cannon" : markerCannon,
+		"bunker" : markerBunker,
+		"locomotive" : markerLocomotive,
+		"tomb" : markerTomb,
+		"grave" : markerGrave,
+		"church" : markerChurch,
+		"bridge" : markerBridge,
+		"city_gate" : markerCityGate,
+		"pillory" : markerPillory,
+		"place_of_worship" : markerPlaceOfWorship,
+		"chateau" : markerChateau,
+		"chalet" : markerChalet,
+		"bakery" : markerBakery,
+		"border_control" : markerBorderControl
+	}
+
 	// variables definitions
 	var selected
 	map = L.map('mapid').setView([48.6905689, 19.4581682], 8);
 	amenitiesLayer = L.geoJSON(null).addTo(map);
+	
 	hikesLayer = L.geoJSON(null, {
     	onEachFeature: addPopup,
     	style: styleHikeDifficulty
@@ -14,7 +76,12 @@ $(document).ready(function() {
 	  }
 	  // Assign new selected
 	  amenitiesLayer.clearLayers()
-	  amenitiesLayer = L.geoJSON(null).addTo(map);
+	  amenitiesLayer = L.geoJSON(null, {
+	  	onEachFeature: addPopup,
+    	pointToLayer: function(feature, latlng) {
+        	return L.marker(latlng, {icon: markers[feature.properties.f2]});
+        }
+      }).addTo(map);
 	  selected = e.layer
 	  amenities = callApi('api/get/amenities/'+e.layer.feature.properties.f1)
 	  if (amenities.data[0][0].features)
@@ -61,10 +128,10 @@ $(document).ready(function() {
 	function styleHikeDifficulty(feature) {
 		var hikeLengthKm = feature.properties.f3
 		if ((50 < hikeLengthKm) && (hikeLengthKm <= 100)) {
-			return {color: "#cc0000"};
+			return {color: '#cc0000'};
 		}
 		else if (100 < hikeLengthKm) {
-			return {color: "#ff00ff"};
+			return {color: '#ff00ff'};
 		}
 	}
 
@@ -76,8 +143,8 @@ $(document).ready(function() {
     	hikeList.add(element.properties);
 	});
 
-	$( "#hikeList" ).on( "mouseenter", function() {
-  		$( "#hikeList a" ).on( "click", function() {
+	$( '#hikeList' ).on( 'mouseenter', function() {
+  		$( '#hikeList a' ).on( 'click', function() {
   			var geometry = $(this).find('input').text();
   			var latLong = geometry.replace('POINT', '').replace('(', '').replace(')', '').split(' ');
   			map.setView({lat: parseFloat(latLong[1]), lng: parseFloat(latLong[0])}, 13);
